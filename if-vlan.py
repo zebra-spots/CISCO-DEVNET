48d81539c279aa394c59d7efdf0f1d6c7b430312
zebra-spots <88425510+zebra-spots@users.noreply.github.com>
2025-12-29 14:51:48 +0000

    Delete if-vlan.py

--- if-vlan.py
+++
@@ -1,7 +0,0 @@
-nativeVLAN = 1
-dataVLAN = 100
-
-if nativeVLAN == dataVLAN:
-    print("The native VLAN and the data VLAN are the same.")
-else:
-    print("The native VLAN and the data VLAN are different.")
