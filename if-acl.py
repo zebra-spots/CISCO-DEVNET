8122fec0526f856549e457d8d60454114e44e62c
zebra-spots <88425510+zebra-spots@users.noreply.github.com>
2025-12-29 14:51:29 +0000

    Delete if-acl.py

--- if-acl.py
+++
@@ -1,7 +0,0 @@
-aclNUM = int(input("What is the IPv4 ACL number? "))
-if aclNUM >= 1 and aclNUM <= 99:
-    print("This is a standard IPv4 ACL.")
-elif aclNUM >=100 and aclNUM <= 199:
-    print("This is an extended IPv4 ACL.")
-else:
-    print("This is not a standard or extended IPv4 ACL.")
