59e5f305625c9c379f14fc824ab60aee2abf90d0
zebra-spots <88425510+zebra-spots@users.noreply.github.com>
2025-12-29 15:01:07 +0000

    Create parsejson.py

---
+++ parsing/parsejson.py
@@ -0,0 +1,13 @@
+import json
+import yaml
+
+with open('myfile.json','r') as json_file:
+    ourjson = json.load(json_file)
+
+#print(ourjson)
+
+#print("The access token is: {}".format(ourjson['access_token']))
+#print("The token expires in: {}".format(ourjson['expires_in']))
+
+print("\n\n---")
+print(yaml.dump(ourjson))
