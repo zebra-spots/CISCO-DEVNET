eca314bd38eef53665dd1c17efb9103830a898f1
zebra-spots <88425510+zebra-spots@users.noreply.github.com>
2025-12-29 15:03:00 +0000

    Update parsejson.py

--- parsing/parsejson.py
+++ parsing/parsejson.py
@@ -1,13 +1,17 @@
+# Import the json and yaml libraries
 import json
 import yaml
 
+# use the json.load method to load the JSON file into a string set to the variable name ourjson
 with open('myfile.json','r') as json_file:
     ourjson = json.load(json_file)
 
 #print(ourjson)
 
+# Add print statements that display the token value and how many seconds until the token expire
 #print("The access token is: {}".format(ourjson['access_token']))
 #print("The token expires in: {}".format(ourjson['expires_in']))
 
+# Output the parsed JSON data in a YAML data format
 print("\n\n---")
 print(yaml.dump(ourjson))
